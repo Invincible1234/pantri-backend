7e7f6774ccf046c040bd71662e35da5411a2a1ee
Jeet Vyas <jeetvyas350@gmail.com>
2026-02-28 12:00:32 -0800

    Check 123

--- src/typescript/controllers/index.ts
+++
@@ -1,9 +0,0 @@
-export class IndexController {
-    public getIndex(req, res) {
-        res.send("Welcome to Pantri!");
-    }
-
-    public getHealth(req, res) {
-        res.status(200).send({ status: "OK" });
-    }
-}
